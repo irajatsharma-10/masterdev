1ae26f18857b0bb0fd3f1367a5760c90c73b9983
irajatsharma-10 <rajat103982@gmail.com>
2024-10-10 23:55:19 +0530

    bcrypt removed

--- server/Controllers/Auth.js
+++ server/Controllers/Auth.js
@@ -1,4 +1,4 @@
-const bcrypt = require("bcrypt")
+const bcrypt = require("bcryptjs")
 const User = require("../Models/User")
 const OTP = require("../Models/OTP")
 const jwt = require("jsonwebtoken")
--- server/Controllers/ResetPassword.js
+++ server/Controllers/ResetPassword.js
@@ -1,7 +1,7 @@
 const User = require("../Models/User");
 const mailSender = require('../Utils/mailsender');
 const crypto = require('crypto');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 
 // resetPasswordToken
